29a34c82c22307798171d9243a2a5e5eaab2c5ba
Mustapha Haadi <mustaphahaadi04@gmail.com>
2026-03-02 18:58:35 +0000

    feat: Introduce Devops practices ( Docker containerization, github actions, code validatiosn) dine user models and admin settings.

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 from .models import User
@@ -10,25 +9,54 @@ from .models import User
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
     """Admin configuration for custom User model."""
-    
-    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined')
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'is_verified')
-    search_fields = ('email', 'first_name', 'last_name')
-    ordering = ('-date_joined',)
-    
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'phone_number', 'avatar', 'bio')}),
-        (_('Permissions'), {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 'is_verified', 'groups', 'user_permissions'),
-        }),
-        (_('Settings'), {'fields': ('email_notifications',)}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+        "date_joined",
     )
-    
+    list_filter = ("is_staff", "is_superuser", "is_active", "is_verified")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("-date_joined",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Personal info"),
+            {"fields": ("first_name", "last_name", "phone_number", "avatar", "bio")},
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "is_verified",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Settings"), {"fields": ("email_notifications",)}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name'),
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "first_name",
+                    "last_name",
+                ),
+            },
+        ),
     )
